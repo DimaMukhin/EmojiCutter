938321a8e3b83ac5c7348b2253f67d156fc1f376
DimaMukhin <dima9348@gmail.com>
2018-07-17 19:41:17 -0700

    refactored zipper service

--- server/services/zipper.js
+++ server/services/zipper.js
@@ -4,6 +4,11 @@ const path = require('path');
 
 const zipper = {};
 
+/**
+ * zip a group of emojis by the name of the emoji
+ * all emojis should be in /image-in/<emoji-name/
+ * @param {string} imageName the name of the large emoji to zip
+ */
 zipper.zipImage = (imageName) => {
     return new Promise((resolve, reject) => {
         console.log('creating writing stream');
