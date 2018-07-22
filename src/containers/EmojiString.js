621b1b2fc2d9628f66d64af014dcf79918d5b274
DimaMukhin <dima9348@gmail.com>
2018-07-22 13:14:35 -0700

    emoji string layout

--- src/containers/EmojiString.js
+++ src/containers/EmojiString.js
@@ -1,12 +1,51 @@
 import React, { Component } from 'react';
 
+import CircleSpace from '../components/CircleSpace';
+
 class EmojiString extends Component {
     render() {
         return (
-            <div>
-                Hello Emoji String container
+            <div style={styles.emojiStringContainer}>
+                <h1 style={styles.emojiStringHeading}>Upload Emoji to Slack</h1>
+                <div style={styles.emojiStringBody}>
+                    <div style={styles.emojiStringBodyLeft}>
+                        <CircleSpace style={styles.circleStepStyle}>
+                            <h1>3</h1>
+                        </CircleSpace>
+                    </div>
+                    <div>
+
+                    </div>
+                    <div>
+
+                    </div>
+                </div>
             </div>
         );
+    }
+}
+
+const styles = {
+    emojiStringContainer: {
+        textAlign: 'center',
+    },
+    emojiStringHeading: {
+        textAlign: 'center'
+    },
+    emojiStringBody: {
+        display: 'grid',
+        gridTemplateColumns: '1fr 1fr 1fr',
+    },
+    emojiStringBodyLeft: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    circleStepStyle: {
+        width: 100,
+        height: 100,
+        color: 'white',
+        backgroundColor: '#9bc1ff'
     }
 }
 
