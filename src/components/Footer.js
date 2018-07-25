16dcfbfc43db8bc96870a0c0d03cf38c0976b89a
DimaMukhin <dima9348@gmail.com>
2018-07-24 19:47:57 -0700

    fixed link security issue

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,9 @@ import packageJson from '../../package.json';
 const Footer = () => {
     return (
         <div>
-            <h3>Made by Dima Mukhin <a href='https://github.com/DimaMukhin' target='_blank'>GitHub</a> | <a href='https://linkedin.com/in/dima-mukhin' target='_blank'>LinkedIn</a></h3>
+            <h3>Made by Dima Mukhin 
+                <a href='https://github.com/DimaMukhin' rel="noopener noreferrer" target='_blank'>GitHub</a> | 
+                <a href='https://linkedin.com/in/dima-mukhin' rel="noopener noreferrer" target='_blank'>LinkedIn</a></h3>
             <p>Alpha {packageJson.version}</p>
         </div>
     );
