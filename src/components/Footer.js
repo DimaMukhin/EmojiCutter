db973a11bb71a7bc32742d0eb210d351b4f62bb2
DimaMukhin <dima9348@gmail.com>
2018-12-24 13:36:02 -0800

    eslinting

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,30 +1,31 @@
 import React from 'react';
 import packageJson from '../../package.json';
 
-const Footer = () => {
-  return (
-    <div>
-      <h3>
-        Made by Dima Mukhin &nbsp;
-        <a
-          href="https://github.com/DimaMukhin"
-          rel="noopener noreferrer"
-          target="_blank"
-        >
-          GitHub
-        </a>{' '}
-        |
-        <a
-          href="https://linkedin.com/in/dima-mukhin"
-          rel="noopener noreferrer"
-          target="_blank"
-        >
-          LinkedIn
-        </a>
-      </h3>
-      <p>Beta {packageJson.version}</p>
-    </div>
-  );
-};
+const Footer = () => (
+  <div>
+    <h3>
+      Made by Dima Mukhin &nbsp;
+      <a
+        href="https://github.com/DimaMukhin"
+        rel="noopener noreferrer"
+        target="_blank"
+      >
+        GitHub
+      </a>{' '}
+      |
+      <a
+        href="https://linkedin.com/in/dima-mukhin"
+        rel="noopener noreferrer"
+        target="_blank"
+      >
+        LinkedIn
+      </a>
+    </h3>
+    <p>
+      Beta
+      {packageJson.version}
+    </p>
+  </div>
+);
 
 export default Footer;
