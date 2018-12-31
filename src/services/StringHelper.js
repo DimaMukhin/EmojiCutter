07b300b62409c1d1a58b17e715cf1a08bfe7b29b
DimaMukhin <dima9348@gmail.com>
2018-12-31 16:25:00 -0600

    final eslint refactoring

--- src/services/StringHelper.js
+++ src/services/StringHelper.js
@@ -1,8 +1,8 @@
 const stringHelper = {};
 
 stringHelper.truncate = (string, maxLength) => {
-  if (string.length > maxLength) return string.substring(0, maxLength) + '...';
-  else return string;
+  if (string.length > maxLength) return `${string.substring(0, maxLength)}...`;
+  return string;
 };
 
 module.exports = stringHelper;
